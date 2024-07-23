97c912ded99a3a615c6f34825a426b54f0ecf40d
Matteo Sabatino <matteo.sabatino@dechit.it>
2024-07-23 17:40:36 +0200

    Add safe area

--- lib/UI/SearchResults.dart
+++ lib/UI/SearchResults.dart
@@ -16,7 +16,6 @@ class SearchResults extends StatefulWidget {
 }
 
 class _SearchResultsState extends State<SearchResults> {
-  final _networkManager = NetworkManager();
   String searchText = "";
 
   void onQueryChanged(String text) {
@@ -27,70 +26,76 @@ class _SearchResultsState extends State<SearchResults> {
   Widget build(BuildContext context) {
     return BlocBuilder<FilterBloc, FilterState>(
         builder: (BuildContext context, FilterState state) {
-          if (state is UpdateErrorState) {
-            return Column(children: [
-              SearchBar(onChanged: onQueryChanged),
-              const SizedBox(height: 20),
-              Expanded(child: Text(state.error))
-            ]);
-          }
+      if (state is UpdateErrorState) {
+        return SafeArea(
+          child: Column(children: [
+            SearchBar(onChanged: onQueryChanged),
+            const SizedBox(height: 20),
+            Expanded(child: Text(state.error))
+          ]),
+        );
+      }
       if (state is InitialState) {
-        return Column(children: [
-          SearchBar(onChanged: onQueryChanged),
-          const SizedBox(height: 20),
-          Expanded(child: Container())
-        ]);
+        return SafeArea(
+          child: Column(children: [
+            SearchBar(onChanged: onQueryChanged),
+            const SizedBox(height: 20),
+            Expanded(child: Container())
+          ]),
+        );
       }
       if (state is UpdateState) {
-        return Column(children: [
-          SearchBar(onChanged: onQueryChanged),
-          const SizedBox(height: 20),
-          Row(
-            children: [
-              TextButton(
-                style: ButtonStyle(
-                  foregroundColor:
-                      MaterialStateProperty.all<Color>(Colors.blue),
+        return SafeArea(
+          child: Column(children: [
+            SearchBar(onChanged: onQueryChanged),
+            const SizedBox(height: 20),
+            Row(
+              children: [
+                TextButton(
+                  style: ButtonStyle(
+                    foregroundColor:
+                        MaterialStateProperty.all<Color>(Colors.blue),
+                  ),
+                  onPressed: () {
+                    context
+                        .read<FilterBloc>()
+                        .add(FilterListEvent("feature-movie"));
+                  },
+                  child: Text('MOVIE'),
                 ),
-                onPressed: () {
-                  context
-                      .read<FilterBloc>()
-                      .add(FilterListEvent("feature-movie"));
-                },
-                child: Text('MOVIE'),
-              ),
-              TextButton(
-                style: ButtonStyle(
-                  foregroundColor:
-                      MaterialStateProperty.all<Color>(Colors.blue),
+                TextButton(
+                  style: ButtonStyle(
+                    foregroundColor:
+                        MaterialStateProperty.all<Color>(Colors.blue),
+                  ),
+                  onPressed: () {
+                    context.read<FilterBloc>().add(FilterListEvent("song"));
+                  },
+                  child: Text('MUSIC'),
                 ),
-                onPressed: () {
-                  context.read<FilterBloc>().add(FilterListEvent("song"));
-                },
-                child: Text('MUSIC'),
-              ),
-              TextButton(
-                style: ButtonStyle(
-                  foregroundColor:
-                      MaterialStateProperty.all<Color>(Colors.blue),
-                ),
-                onPressed: () {
-                  context.read<FilterBloc>().add(ResetFilterEvent());
-                },
-                child: Text('RESET'),
-              )
-            ],
-          ),
-          Expanded(
-              child: ListView.builder(
-            scrollDirection: Axis.vertical,
-            shrinkWrap: true,
-            itemCount: state.result.length,
-            itemBuilder: (context, index) {
-              return TableContainerView(result: state.result[index]);
-            },
-          ))
-        ]);
+                TextButton(
+                  style: ButtonStyle(
+                    foregroundColor:
+                        MaterialStateProperty.all<Color>(Colors.blue),
+                  ),
+                  onPressed: () {
+                    context.read<FilterBloc>().add(ResetFilterEvent());
+                  },
+                  child: Text('RESET'),
+                )
+              ],
+            ),
+            Expanded(
+                child: ListView.builder(
+              scrollDirection: Axis.vertical,
+              shrinkWrap: true,
+              itemCount: state.result.length,
+              itemBuilder: (context, index) {
+                return TableContainerView(result: state.result[index]);
+              },
+            ))
+          ]),
+        );
       }
       return Container();
     });
