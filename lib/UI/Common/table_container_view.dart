09694eb9e68a2b7cd5ac1ca5c5b6bb7a777fb53b
Gabriele Lenti <gabriele.lenti@dechit.it>
2024-07-26 12:45:56 +0200

    Aggiunte immagini custom

--- lib/UI/Common/table_container_view.dart
+++ lib/UI/Common/table_container_view.dart
@@ -21,46 +21,59 @@ class TableContainerView extends StatelessWidget {
     String imageUrl = result.artworkUrl100 ?? "";
 
     return Container(
-      padding: const EdgeInsets.all(8),
-      child: Row(
-        children: [
-          ClipRRect(
-            borderRadius: BorderRadius.circular(8.0),
-            child: Image.network(
-              imageUrl,
-              height: 100.0,
-              width: 100.0,
-              fit: BoxFit.fill,
+      padding: const EdgeInsets.fromLTRB(8, 4, 8, 4),
+      child: Card(
+        elevation: 3,
+        shadowColor: Colors.white54,
+        color: Colors.white,
+        child: Row(
+          children: [
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Card(
+                elevation: 2,
+                shadowColor: Colors.grey,
+                child: ClipRRect(
+                  borderRadius: BorderRadius.circular(8.0),
+                  child: FadeInImage.assetNetwork(
+                    placeholder: 'lib/assets/placeholder.jpg', // può essere anche .gif
+                    image: imageUrl,
+                    height: 100.0,
+                    width: 100.0,
+                    fit: BoxFit.fill,
+                  ),
+                ),
+              ),
             ),
-          ),
-          const SizedBox(
-            width: 20,
-          ),
-          Expanded(
-              child: Text.rich(TextSpan(
-            children: [
-              TextSpan(
-                  text: artistName,
-                  style: const TextStyle(fontWeight: FontWeight.bold)),
-              TextSpan(text: ' - $albumName'),
-            ],
-          ))),
-          Padding(
-            padding: const EdgeInsets.fromLTRB(8, 0, 8, 0),
-            child: TextButton(
-              onPressed: () {
-                favoritesProvider.addOrRemoveFavorite(trackId);
-              },
-              child: favoritesProvider.isElementInFavorite(trackId) ? const Icon(
-                  Icons.favorite,
-                  color: Colors.deepPurple
-              ) : const Icon(
-                  Icons.favorite_border_outlined,
-                  color: Colors.deepPurple
-              ) ,
+            const SizedBox(
+              width: 20,
             ),
-          ),
-        ],
+            Expanded(
+                child: Text.rich(TextSpan(
+              children: [
+                TextSpan(
+                    text: artistName,
+                    style: const TextStyle(fontWeight: FontWeight.bold)),
+                TextSpan(text: ' - $albumName'),
+              ],
+            ))),
+            Padding(
+              padding: const EdgeInsets.fromLTRB(8, 0, 8, 0),
+              child: TextButton(
+                onPressed: () {
+                  favoritesProvider.addOrRemoveFavorite(trackId);
+                },
+                child: favoritesProvider.isElementInFavorite(trackId) ? const Icon(
+                    Icons.favorite,
+                    color: Colors.deepPurple
+                ) : const Icon(
+                    Icons.favorite_border_outlined,
+                    color: Colors.deepPurple
+                ) ,
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
